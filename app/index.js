eb7a47434addbbc062a5f2a8c9cd048c79d3f066
Frey Hargarten <jeff.hargarten@startribune.com>
2018-08-01 15:48:08 -0500

    push

---
+++ app/index.js
@@ -0,0 +1,50 @@
+/**
+ * Main JS file for project.
+ */
+
+// Define globals that are added through the config.json file, here like this:
+// /* global _ */
+'use strict';
+
+// Dependencies
+import utilsFn from './utils.js';
+import * as d3 from 'd3';
+import * as c3 from 'c3';
+import Map from './chart.js';
+
+const map = new Map('#chartCounts');
+const chart1 = new Map('#chartNP');
+const chart2 = new Map('#chartRate');
+
+// Import local ES6 modules like this:
+//import utilsFn from './utils.js';
+
+// Or import libraries installed with npm like this:
+// import module from 'module';
+
+// Setup utils function
+utilsFn({});
+
+$.urlParam = function(name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results != null) {
+        return results[1] || 0;
+    } else {
+        return null;
+    }
+}
+
+var selected = $.urlParam('chart');
+
+if (selected != null) {
+    $(".slide").hide();
+    $("#" + selected).show();
+}
+if (selected == "all") {
+    $(".slide").show();
+}
+
+
+map.render();
+chart1.render();
+chart2.render();
